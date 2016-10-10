a75cec53a534528ad7a6bc573b7c9521a1868f47
Kaleb Davis <kalebdavisgithub@gmail.com>
2016-10-10 18:09:18 -0400

    feat(app): Add express boilerplate

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Escalator-API
+
+## Set Up
+
+`npm install`
+
+`gulp serve`
+
+Refer to [this](https://github.com/KunalKapadia/express-mongoose-es6-rest-api)
+for instructions on how to set up, run tests, etc.
---
+++ index.js
@@ -0,0 +1,22 @@
+import Promise from 'bluebird';
+import mongoose from 'mongoose';
+import config from './config/env';
+import app from './config/express';
+
+// plugin bluebird promise in mongoose
+mongoose.Promise = Promise;
+
+// connect to mongo db
+mongoose.connect(config.db, { server: { socketOptions: { keepAlive: 1 } } });
+mongoose.connection.on('error', () => {
+  throw new Error(`unable to connect to database: ${config.db}`);
+});
+
+const debug = require('debug')('escalator-api:index');
+
+// listen on port config.port
+app.listen(config.port, () => {
+  debug(`server started on port ${config.port} (${config.env})`);
+});
+
+export default app;
