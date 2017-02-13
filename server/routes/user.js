483e047cb98cff066123a324ed3bd93328cb6335
Kaleb Davis <kalebdavisgithub@gmail.com>
2017-02-13 16:54:17 -0500

    Remove unnecessary spaces for readability

--- server/routes/user.js
+++ server/routes/user.js
@@ -6,33 +6,23 @@ import userCtrl from '../controllers/user';
 const router = express.Router();  // eslint-disable-line new-cap
 
 router.route('/')
-
   .post(validate(paramValidation.createUser), userCtrl.createUser);
 
 router.route('/:userId')
-
   .get(validate(paramValidation.byId), userCtrl.getUser)
-
   .put(validate(paramValidation.updateUser), userCtrl.updateUser)
-
   .delete(validate(paramValidation.byId), userCtrl.deleteUser);
 
 router.route('/:userId/device')
-
   .post(validate(paramValidation.addDevice), userCtrl.addDevice);
 
 router.route('/:userId/device/:deviceId')
-
   .get(validate(paramValidation.deviceById), userCtrl.getDevice)
-
   .put(validate(paramValidation.updateDevice), userCtrl.updateDevice)
-
   .post(validate(paramValidation.deviceById), userCtrl.sortDevices)
-
   .delete(validate(paramValidation.deviceById), userCtrl.removeDevice);
 
 router.route('/:userId/group')
-
   .get(validate(paramValidation.byId), userCtrl.getGroupsForUser);
 
 /** Load user when API with userId route parameter is hit */
